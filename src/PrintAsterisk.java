a5f5e509ef36f876779926970daf6ded7f82e115
aalmeidatw <aalmeida@thoughtworks.com>
2016-02-17 15:29:22 -0200

    first commit

---
+++ src/PrintAsterisk.java
@@ -0,0 +1,18 @@
+
+
+
+public class PrintAsterisk {
+
+     public boolean printerAsteriskOnConsole(String  value) {
+
+         if (value.equals("*")) {
+             System.out.print(value);
+             return true;
+         }
+
+         return false;
+    }
+
+
+
+}
